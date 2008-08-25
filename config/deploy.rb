580a0b4b6b857a844690cc391ac564c0b58a7170
Zack Ham <zackham@gmail.com>
2008-08-25 11:43:11 -0700

    updates to layout/deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,45 @@ task :after_update_code do
   deploy.cleanup
 end
 
+set :mysql_username, 'tf_user'
+set :mysql_password, 'fv38vdl2'
+set :mysql_production_db, 'ticketfast_production'
+set :mysql_host, '10.10.1.11'
+
+def run_local cmd
+  puts %[  * executing locally "#{cmd}"]
+  `#{cmd}`
+end
+
+namespace :dev do
+  desc 'Retrieve current production database and import locally.'
+  task :import_production_db do 
+    n = Time.now
+
+    file = "ticketfast-#{sprintf("%d%02d%02d%02d%02d", n.year, n.month, n.day, n.hour, n.min)}.sql"
+    remote_path = "/root/#{file}"
+
+    on_rollback { run "rm #{remote_path}" }
+
+    run "mysqldump -u #{mysql_username} -p #{mysql_production_db} -h #{mysql_host} > #{remote_path}" do |ch, stream, out|
+      ch.send_data "#{mysql_password}\n" if out =~ /^Enter password:/
+    end
+
+    run_local "rsync #{user}@#{roles[:db].servers.first.host}:#{remote_path} ."
+    run "rm #{remote_path}"
+
+    run_local "rake db:drop"
+    run_local "rake db:create"
+    run_local "mysql -u root ticketfast_development < #{file}"
+    run_local "rake db:migrate"
+  end
+  
+  desc 'Copy photos from production server'
+  task :import_pdfs do
+    run_local "rsync -r #{user}@#{roles[:db].servers.first.host}:/var/www/ticketfast.neco.com/current/pdfs/* pdfs/"
+  end
+end
+
 namespace :deploy do
   namespace :mongrel do
     [ :stop, :start, :restart ].each do |t|
