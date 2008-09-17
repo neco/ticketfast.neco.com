d9fd1b1549fd0ef26ad22005ba0e8f7a690249a2
Zack Ham <zackham@gmail.com>
2008-09-16 17:11:49 -0700

    updated worker

--- lib/workers/job_queue_worker.rb
+++ lib/workers/job_queue_worker.rb
@@ -38,51 +38,43 @@ class JobQueueWorker < BackgrounDRb::MetaWorker
   
   # This assumes a job never ever fails, is this a problem?
   def get_job(remote_ip)
-    @mutex.synchronize {
-      if(next_work_time - Time.now < 5 and !working?) 
-        logger.debug "Starting work!"
-        start_work
-      end
-      if @targetted_jobs[remote_ip] and @targetted_jobs[remote_ip].size > 0
-        logger.debug "Getting targetted job for #{remote_ip}"
-        return @targetted_jobs[remote_ip].shift
-      end
-      @jobs.shift || {:action => :sleep, :duration => ((next_work_time - Time.now < 30) || working? ? 5 : 30)}
-    }
+    if(next_work_time - Time.now < 5 and !working?) 
+      logger.debug "Starting work!"
+      start_work
+    end
+    if @targetted_jobs[remote_ip] and @targetted_jobs[remote_ip].size > 0
+      logger.debug "Getting targetted job for #{remote_ip}"
+      return @targetted_jobs[remote_ip].shift
+    end
+    @jobs.shift || {:action => :sleep, :duration => ((next_work_time - Time.now < 30) || working? ? 5 : 30)}
   end
   
   def submit_work(args)
-    @mutex.synchronize {
-      logger.debug "** submitting work for client key #{args[:client_key]} and ip #{args[:remote_ip]}" 
-      client_key, results = args[:client_key], args[:results]
-      @job_results[client_key] = results.merge(:remote_ip => args[:remote_ip])
-      logger.debug "** work received"
-    }
+    logger.debug "** submitting work for client key #{args[:client_key]} and ip #{args[:remote_ip]}" 
+    client_key, results = args[:client_key], args[:results]
+    @job_results[client_key] = results.merge(:remote_ip => args[:remote_ip])
+    logger.debug "** work received"
   end
   
   def fetch_request(args)
-    @mutex.synchronize {
-      logger.debug "** fetching request: #{args.reject{|k,v| k == :options}.inspect}"
-      client_key, uri, options = args[:client_key], args[:uri], args[:options]
-      job_data = {:action => :fetch_request, :client_key => client_key, :uri => uri, :options => options}
-      if args[:job_target]
-        logger.debug "Creating targetted job for #{args[:job_target]}"
-        @targetted_jobs[args[:job_target]] ||= []
-        @targetted_jobs[args[:job_target]] << job_data
-      else
-        logger.debug "Creating normal job"
-        @jobs << job_data
-      end
-      @job_results[client_key] = false
-    }
+    logger.debug "** fetching request: #{args.reject{|k,v| k == :options}.inspect}"
+    client_key, uri, options = args[:client_key], args[:uri], args[:options]
+    job_data = {:action => :fetch_request, :client_key => client_key, :uri => uri, :options => options}
+    if args[:job_target]
+      logger.debug "Creating targetted job for #{args[:job_target]}"
+      @targetted_jobs[args[:job_target]] ||= []
+      @targetted_jobs[args[:job_target]] << job_data
+    else
+      logger.debug "Creating normal job"
+      @jobs << job_data
+    end
+    @job_results[client_key] = false
   end
   
   def fetch_response(args)
-    @mutex.synchronize {
-      client_key = args[:client_key]
-      logger.debug "job status of #{client_key}: #{@job_results[client_key] ? 'received' : 'waiting'}"
-    
-      @job_results[client_key]
-    }
+    client_key = args[:client_key]
+    logger.debug "job status of #{client_key}: #{@job_results[client_key] ? 'received' : 'waiting'}"
+  
+    @job_results[client_key]
   end
 end
