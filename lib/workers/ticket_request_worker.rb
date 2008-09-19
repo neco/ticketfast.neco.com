1bb871ed0a90825f83a1398b209a8148b292116b
Zack Ham <zackham@gmail.com>
2008-09-19 10:04:07 -0700

    avoid exception

--- lib/workers/ticket_request_worker.rb
+++ lib/workers/ticket_request_worker.rb
@@ -96,7 +96,8 @@ class TicketRequestWorker < BackgrounDRb::MetaWorker
           
           if unfetched_order_numbers.size > 0
             unfetched_order_numbers.each {|num, date|
-              TmAccount.find(tm_account_id).tickets.unfetched.find_by_order_number(num).update_attribute(:unfetched_reason, 'No longer listed in order history.')
+                tick = TmAccount.find(tm_account_id).tickets.unfetched.find_by_order_number(num)
+                tick.update_attribute(:unfetched_reason, 'No longer listed in order history.') if tick
             }
           end
     
