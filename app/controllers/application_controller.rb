3d63f3d543934e94271880a33b10795a41eedc6b
Tyler Hunt <tyler@tylerhunt.com>
2009-10-30 15:21:37 -0400

    Implemented HTTP Basic authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
   before_filter :assign_empty_js_includes
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'neco' && password == 'hurricane123'
+    end
+  end
+  protected :authenticate
 
   def assign_empty_js_includes
     @js_includes = []
   end
+  protected :assign_empty_js_includes
 end
